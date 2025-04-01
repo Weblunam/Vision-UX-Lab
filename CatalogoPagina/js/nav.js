7629980cebb4c55e365477f045ab57bb7aac1125
soffuyuu <sofiaterrazasv@gmail.com>
2025-04-01 16:09:08 -0600

    Avance diseño responsivo - página de inicio

--- CatalogoPagina/js/nav.js
+++ CatalogoPagina/js/nav.js
@@ -1,20 +1,48 @@
-window.addEventListener('scroll', function () {
-    var barraNav = document.querySelector('nav');
-    var txtNav = document.querySelectorAll('nav ul li a');
-    if (window.scrollY > 100) {
-        barraNav.style.backgroundColor = 'rgba(255, 255, 255, 0.65)';
-        barraNav.style.backdropFilter = 'blur(9px)';
-        txtNav.forEach(texto => {
-            texto.style.fontWeight = "500";
-        });
-    } else {
-        barraNav.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-        barraNav.style.backdropFilter = 'none';
-        txtNav.forEach(texto => {
-            texto.style.fontWeight = "400";
-        });
-    }
+var barraNav = document.querySelectorAll('nav');
+var nav2 = document.querySelector('#nav-responsiva');
+var txtNavResponsiva = document.querySelectorAll('#nav-responsiva ul li a');
+var menu = document.querySelector('#menu-icon');
+var close = document.querySelector('#close-icon');
+
+barraNav.forEach(barra => {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 100) {
+            barra.style.backgroundColor = 'rgba(255, 255, 255, 0.65)';
+            barra.style.backdropFilter = 'blur(9px)';
+        } else {
+            barra.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+            barra.style.backdropFilter = 'none';
+        }
+    });
 });
+
+//nav responsiva
+
+function abrirNavResponsiva() {
+    nav2.classList.add("estilo-nav-responsiva");
+    txtNavResponsiva.forEach(texto => {
+        texto.style.display = "block";
+        setTimeout(() => {
+            texto.style.opacity = "1";
+        }, 250);
+    });
+    close.style.display = "block";
+    menu.style.display = "none";
+}
+
+function cerrarNavResponsiva() {
+    nav2.classList.remove("estilo-nav-responsiva");
+    close.style.display = "none";
+    txtNavResponsiva.forEach(texto => {
+        texto.style.opacity = "0";
+        setTimeout(() => {
+            texto.style.display = "none";
+        }, 250);
+    });
+    menu.style.display = "block";
+}
+
+
 
 
 
