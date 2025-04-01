0815778f8c8e04e0245a656f65b7b1970bf37ea4
soffuyuu <sofiaterrazasv@gmail.com>
2025-03-31 20:28:58 -0600

    pagina de recursos terminada:D

--- CatalogoPagina/js/parallax.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let parallax = document.querySelector('#parallax');
-    let scrollY = window.scrollY;
-    parallax.style.right = scrollY * 0.35 + "px";
-});
